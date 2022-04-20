92f87d476a53f417a88c1139530d92a4b4d56bdb
Artur Ferreira Moreira <artur31415926@gmail.com>
2022-04-20 11:56:10 -0300

    click code

--- Scenes/Territory2D.gd
+++ Scenes/Territory2D.gd
@@ -8,9 +8,23 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print("haha")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_StaticBody2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton \
+		and event.button_index == BUTTON_LEFT \
+		and event.pressed:
+		print("here1")
+
+	if event is InputEventScreenTouch and event.is_pressed():
+		print("here3")
+
+
+func _on_StaticBody2D_mouse_entered():
+	print("here2")
